f96bad08c6c35002981d12164fc0bbfe961c3f1d
Lionnel Mazuba <lionnelmaz@icloud.com>
2022-09-08 18:41:31 -0700

    solve betty problem

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -8,7 +7,6 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(message, strlen(message), 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	return (1);
 }
