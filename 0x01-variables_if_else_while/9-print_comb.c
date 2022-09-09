2c19f940924037adaf98a1f557dfbf5e71d0353b
Lionnel Mazuba <lionnelmaz@icloud.com>
2022-09-09 04:08:27 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,13 @@
 int main(void)
 {
 	int n = 0;
-	
-	while(n < 10)
+
+	while (n < 10)
 	{
-		putchar(n+'0');
-		if(n != 9)
-		{	
+		putchar(n + '0');
+		if (n != 9)
+		{
+
 			putchar(',');
 			putchar(' ');
 		}
