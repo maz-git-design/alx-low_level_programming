716975213939498d33e59488bb456c3995215b1e
Lionnel Mazuba <lionnelmaz@icloud.com>
2022-09-14 14:00:59 -0700

    task 12 redone

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, product, first, second, third;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -27,22 +27,22 @@ void print_times_table(int n)
 					}
 					else if (product >= 10 && product < 100)
 					{
-						first = product / 10;
-						second = product % 10;
+						first = product / 10, second = product % 10;
 						_putchar(' ');
 						_putchar(first + '0');
 						_putchar(second + '0');
 					}
 					else if (product >= 100)
 					{
-						first = product / 100;
-						second = (product / 10) % 10;
+						first = product / 100, second = (product / 10) % 10;
 						third = product % 10;
 						_putchar(first + '0');
 						_putchar(second + '0');
 						_putchar(third + '0');
 					}
 				}
+				else
+					_putchar(product + '0');
 			}
 			_putchar('\n');
 		}
