8bdae93039e021693c1bc433afe2f56f371d7f8f
Lionnel Mazuba <lionnelmaz@icloud.com>
2022-09-16 17:19:05 -0700

    task 11 done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,11 +6,36 @@
  */
 int main(void)
 {
-	int i;
-	int factor;
+	long  i, j;
+	long  prime, n;
 
-	i = 2;
-	
+	n = 612852475143;
+	while (n > 1)
 	{
+		for (i = 2; i <= n; i++)
+		{
+			for (j = 2; j <= i; j++)
+			{
+				if (i % j == 0 && j != i)
+				{
+					prime = 0;
+					break;
+				}
+				if (j == i)
+					prime = i;
+			}
+
+			if (prime)
+			{
+				if (n % prime == 0)
+				{
+					n = n / prime;
+					if (n == 1)
+						printf("%ld\n", prime);
+					break;
+				}
+			}
+		}
 	}
+	return (0);
 }
