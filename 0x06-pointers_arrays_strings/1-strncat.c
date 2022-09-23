f08d0c32706aabc99b135e89364f549d9b64a44d
Lionnel Mazuba <lionnelmaz@icloud.com>
2022-09-23 16:50:06 -0700

    task 1 redone

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,27 +8,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	char c;
+	int i;
+	int  dest_len;
 
 	i = 0;
+	dest_len = 0;
 	do {
-		c = *(dest + i);
-		if (c == '\0')
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (*(src + j) != '\0')
-					*(dest + i + j) = *(src + j);
-				else
-				{
-					*(dest + i + j) = '\0';
-					break;
-				}
-			}
-		}
+		dest_len++;
 		i++;
-	} while (c != '\0');
+	} while (*(dest + i) != '\0'); 
 
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
